bc1fa3ae2a12dbe1ef5b332300083a775b529882
Ivan Shevchenko <rayserviz@gmail.com>
2020-09-17 17:55:47 +0300

    add navbar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Navbar from './src/components/Navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <View>
+      <Navbar title="Unsplash" />
     </View>
   );
 }
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  navbar: {
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#aaa',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
