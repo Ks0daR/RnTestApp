14aa553da110fca0f7a699799235154d55301551
Ivan Shevchenko <rayserviz@gmail.com>
2020-09-19 10:56:47 +0300

    remove constants

--- constants.js
+++
@@ -1,4 +0,0 @@
-export const URI =
-  'https://api.unsplash.com/photos/?client_id=ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9';
-setLoading(true);
-export const HEADERS = {'Accept-Version': 'v1'};
